72b73a5bde64c0609bcc3e74ccda4a687d9394ab
Lucía Agulló Martí <luciaagullo@outlook.com>
2025-12-04 10:34:28 +0100

    Fix package maintainer

--- cmake/CPackConfig.cmake
+++ cmake/CPackConfig.cmake
@@ -15,7 +15,7 @@ set(CPACK_GENERATOR "TGZ;DEB")
 set(CPACK_STRIP_FILES TRUE)
 
 # Debian specific settings
-set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Lucia Agullo Marti")
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Lucia Agullo Marti <noreply@example.com>")
 set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "CPack exercise for the Simulation Software Engineering course (Winter Term 2025/2026)")
 set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
